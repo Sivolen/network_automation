f2a9b01f6934f15dcded59a0c41a259648fe103a
Sivolen <anton@gridnev.pro>
2022-01-20 16:17:36 +1100

    Small bug fix

--- main.py
+++ main.py
@@ -1,17 +1,29 @@
-import paramiko
-import time
-import multiprocessing
 import re
-# import ipaddress
-# import subprocess
+import time
+import logging
+import multiprocessing
+
 from datetime import datetime
-# from easysnmp import snmp_walk, Session
+from logging.handlers import RotatingFileHandler
+
 import netsnmp
+import paramiko
 
 from settings import *
 
 # Start time at work
 start_time = datetime.now()
+
+# Configure logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+file_header = RotatingFileHandler("logs/debug.log", maxBytes=100000, backupCount=100)
+file_header.setLevel(logging.DEBUG)
+log_format = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+file_header.setFormatter(log_format)
+logger.addHandler(file_header)
 
 
 def check_ip(ipaddress):
@@ -28,7 +40,7 @@ def get_ip_list():
     try:
         return [line.replace('\n', '') for line in open('ip').readlines() if check_ip(ipaddress=line) is not None]
     except IOError:
-        print('File do not open because ip_full file does not exist')
+        logger.debug('File do not open because ip file does not exist')
 
 
 # SNMP connection to device for get_device_id
@@ -52,7 +64,7 @@ def get_device_id(ipaddress):
             if snmp_walk == ():
                 snmp_walk = snmp_connection(ipaddress, community2)
         except Exception as snmp_error:
-            print(f'{ipaddress}: snmp connections error ' + str(snmp_error))
+            logger.debug(f'{ipaddress}: snmp connections error ' + str(snmp_error))
         if re.search(r'\bCisco\b', str(snmp_walk)):
             vendor_id = 'Cisco'
         elif re.search(r'\bHuawei\b', str(snmp_walk)) or \
@@ -64,10 +76,10 @@ def get_device_id(ipaddress):
 
 
 # Function ssh connectivity and sensing device commands without multiprocessing
-def ssh_connect(ipaddress):
+def connecting_to_devices(ipaddress):
     vendor = get_device_id(ipaddress)
     if vendor is not None:
-        print(colors.BOLD + f"\nStart connection to: {ipaddress} ({vendor})" + colors.ENDC)
+        logger.debug(colors.BOLD + f"\nStart connection to: {ipaddress} ({vendor})" + colors.ENDC)
         client = paramiko.SSHClient()
         client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
         try:
@@ -75,7 +87,7 @@ def ssh_connect(ipaddress):
                            allow_agent=False, timeout=5)
         except (paramiko.AuthenticationException,
                 paramiko.ssh_exception.NoValidConnectionsError, paramiko.SSHException) as connection_error:
-            print(colors.FAIL + f"\nError connecting to {ipaddress}: {connection_error}" + colors.ENDC)
+            logger.debug(colors.FAIL + f"\nError connecting to {ipaddress}: {connection_error}" + colors.ENDC)
         try:
             with client.invoke_shell() as ssh_cli:
                 if vendor == 'Cisco':
@@ -90,7 +102,7 @@ def ssh_connect(ipaddress):
 
                     if re.search(r'\b10.0.0.172\b', result):
                         time.sleep(0.5)
-                        print(f'Tacacs server is old')
+                        logger.debug(f'Tacacs server is old')
                         ssh_cli.send('conf t\n'.encode())
                         time.sleep(0.5)
                         ssh_cli.send('tacacs-server host 10.0.3.14\n'.encode())
@@ -103,10 +115,10 @@ def ssh_connect(ipaddress):
                         time.sleep(1)
                     elif re.search(r'\b10.0.3.14\b', result):
                         time.sleep(0.5)
-                        print(f'{ipaddress}: tacacs server is already new')
+                        logger.debug(f'{ipaddress}: tacacs server is already new')
                     elif re.search(r'\b\b', result):
                         time.sleep(0.5)
-                        print('Tacacs server is not configured')
+                        logger.debug('Tacacs server is not configured')
 
                     print(result)
                     client.close()
@@ -122,32 +134,38 @@ def ssh_connect(ipaddress):
                     result = ssh_cli.recv(99999).decode('ascii')
 
                     if re.search(r'\b10.0.0.172\b', result):
-                        print(f'{ipaddress}: tacacs server is old version')
+                        logger.debug(f'{ipaddress}: tacacs server is old version')
                     elif re.search(r'\b10.0.3.14\b', result):
-                        print(f'{ipaddress}: tacacs server is already new')
+                        logger.debug(f'{ipaddress}: tacacs server is already new')
                     elif re.search(r'\b\b', result):
-                        print(f'{ipaddress}: tacacs server is not configured')
+                        logger.debug(f'{ipaddress}: tacacs server is not configured')
 
                     print(result)
                     client.close()
         except Exception as commands_error:
-            print(f"\nError commands to {ipaddress}: {commands_error}")
-        print(colors.BOLD + f"\nEnd {ipaddress}" + colors.ENDC)
+            logger.debug(f"\nError commands to {ipaddress}: {commands_error}")
+        logger.debug(colors.BOLD + f"\nEnd {ipaddress}" + colors.ENDC)
     else:
-        print(f"What's wrong: device {ipaddress} is not support or your community is wrong")
+        logger.debug(f"What's wrong: device {ipaddress} is not support or your community is wrong")
+
+
+# Main funktion, init multiprocess
+def main():
+    device_ip_list = get_ip_list()
+    print(*device_ip_list)
+
+    multiprocessing.set_start_method("spawn")
+    with multiprocessing.Pool(maxtasksperchild=3) as process_pool:
+        # ssh_connect - function, device_ip_list - argument
+        process_pool.map(connecting_to_devices, device_ip_list, 1)
+
+        process_pool.close()
+        process_pool.join()
 
 
 # Start script execution
 if __name__ == '__main__':
-    device_ip_list = get_ip_list()
-    print(*device_ip_list)
-    #    ssh_connect(device_ip_list, 'sh int desc\nsh ver\n')
-    multiprocessing.set_start_method("spawn")
-    with multiprocessing.Pool(maxtasksperchild=3) as process_pool:
-        routers_with_issues = process_pool.map(ssh_connect, device_ip_list,
-                                               1)  # ssh_connect - function, device_ip_list - argument
-        process_pool.close()
-        process_pool.join()
+    main()
 
     # Print end time work
     print(colors.BOLD + f"\n{datetime.now() - start_time}" + colors.ENDC)
